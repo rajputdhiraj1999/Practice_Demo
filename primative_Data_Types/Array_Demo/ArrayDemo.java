3106ae9eef7b2a72a964bbd41a6396e3305a3b22
rajputdhiraj1999 <rajputdhiraj1999>
2024-09-30 12:37:15 +0530

    Arrey Demo

---
+++ primative_Data_Types/Array_Demo/ArrayDemo.java
@@ -0,0 +1,32 @@
+package primative_Data_Types.Array_Demo;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+        int[] anArray;
+
+        anArray = new int[10];
+        anArray[0] = 50;
+        anArray[1] = 70;
+        anArray[2] = 60;
+        anArray[3] = 80;
+        anArray[4] = 40;
+        anArray[5] = 30;
+        anArray[6] = 110;
+        anArray[7] = 210;
+        anArray[8] = 220;
+        anArray[9] = 400;
+
+        System.out.println("Element at index 0: "+ anArray[0]);
+        System.out.println("Element at index 1: "+ anArray[1]);
+        System.out.println("Element at index 2: "+ anArray[2]);
+        System.out.println("Element at index 3: "+ anArray[3]);
+        System.out.println("Element at index 4: "+ anArray[4]);
+        System.out.println("Element at index 5: "+ anArray[5]);
+        System.out.println("Element at index 6: "+ anArray[6]);
+        System.out.println("Element at index 7: "+ anArray[7]);
+        System.out.println("Element at index 8: "+ anArray[8]);
+        System.out.println("Element at index 9: "+ anArray[9]);
+
+    }
+    
+}
