90d503a9b030d2ae2b1f7658dca32ce75f1015b4
rajputdhiraj1999 <rajputdhiraj1999>
2024-09-30 12:13:53 +0530

    Second Commit

---
+++ primative_Data_Types/DefaultValue.java
@@ -0,0 +1,28 @@
+package primative_Data_Types;
+
+public class DefaultValue {
+
+    static int i;
+    static byte by;
+    static short sh;
+    static long l;
+    static float f;
+    static double d;
+    static char c;
+    static boolean b1;
+    static String s;
+
+    public static void main(String[] args)
+    {
+        System.out.println(i);
+        System.out.println(by);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(s);
+        System.out.println(sh);
+        System.out.println(b1);
+    }
+}
+
